e51998fe8d9209fef489b89989ce4b4a9b35fbea
sunzhandong <sunzhandong@jd.com>
2018-11-25 16:52:22 +0800

    java

---
+++ dasheng_simple/src/main/java/com/dasheng/generic_programming/generic_class/GenericClassTest.java
@@ -0,0 +1,21 @@
+package com.dasheng.generic_programming.generic_class;
+
+import org.junit.Test;
+
+/**
+ * 测试案例
+ * @author sunzhandong
+ */
+public class GenericClassTest {
+
+    /**
+     * 获取最大值最小值
+     */
+    @Test
+    public void compareStr(){
+        String[] words = {"Mary","had","a","little","lamb"};
+        Pair<String> mm = ArrayAlg
+
+    }
+
+}
